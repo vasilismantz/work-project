2d1c3ad8f007d2a131c8d5d89450862d96ab2571
Vasilis Mantzaris <mantzaris.vas98@gmail.com>
2021-01-14 13:08:44 +0100

    [i-32] :sparkles: Add delete Project functionality

--- apps/www/src/components/Projects/Projects.js
+++ apps/www/src/components/Projects/Projects.js
@@ -1,28 +1,53 @@
 import { useState, useEffect } from "react";
 import { useProjects } from "@/hooks";
 import { useMutation, useQuery } from "@apollo/client";
-import { GET_CATEGORIES } from "@work-project/graphql";
+import { GET_CATEGORIES, REMOVE_CATEGORY } from "@work-project/graphql";
 import { Delete } from "@material-ui/icons";
+import { useSnackbar } from "notistack";
 
 const Projects = () => {
-  const { loading, error, data } = useQuery(GET_CATEGORIES);
-  if (error) console.log(`Error! ${error.message}`);
-  if (loading) return "Loading...";
+  const { enqueueSnackbar } = useSnackbar();
+
+  const { data, loading, error, refetch } = useQuery(GET_CATEGORIES, {
+    onError: error => enqueueSnackbar(error.message, { variant: "error" }),
+  });
+
+  const [removeProject, { loading: removeLoading }] = useMutation(
+    REMOVE_CATEGORY,
+    {
+      onCompleted: () => {
+        refetch();
+        enqueueSnackbar("Project removed successfuly.", { variant: "success" });
+      },
+      onError: error => enqueueSnackbar(error.message, { variant: "error" }),
+    }
+  );
+
+  const handleRemoveProject = id => {
+    removeProject({ variables: { id } });
+  };
 
   console.log(data);
 
-  return data.categories.map(project => {
-    return (
-      <li key={project.id} className="sidebar__project">
-        <span className="sidebar__dot">•</span>
-        <span className="sidebar__project-name">{project.name}</span>
-        {/* Add onClick function */}
-        <span className="sidebar__project-delete">
-          <Delete />
-        </span>
-      </li>
-    );
-  });
+  return data ? (
+    data.categories.map(project => {
+      return (
+        <li key={project.id} className="sidebar__project">
+          <span className="sidebar__dot">•</span>
+          <span className="sidebar__project-name">{project.name}</span>
+          {/* Add onClick function */}
+          <span
+            className="sidebar__project-delete"
+            onClick={() => handleRemoveProject(project.id)}
+          >
+            <Delete />
+          </span>
+        </li>
+      );
+    })
+  ) : (
+    <></>
+  );
 };
 
 export default Projects;
