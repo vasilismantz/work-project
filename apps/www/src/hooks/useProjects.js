ee600fae89a110b8f202e2e2b28fc683b9e42cf3
Vasilis Mantzaris <mantzaris.vas98@gmail.com>
2021-01-22 18:27:12 +0100

    [i-32] :recycle: Refactor hook to work properly

--- apps/www/src/hooks/useProjects.js
+++ apps/www/src/hooks/useProjects.js
@@ -1,17 +1,24 @@
 import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_CATEGORIES } from "@work-project/graphql";
+import { useSnackbar } from "notistack";
 
 const useProjects = () => {
-  const [categories, setCategories] = useState([]);
+  const { enqueueSnackbar } = useSnackbar();
+  const [projects, setProjects] = useState([]);
+
+  const { data, error, refetch } = useQuery(GET_CATEGORIES, {
+    onError: error => enqueueSnackbar(error.message, { variant: "error" }),
+    onCompleted: () => {
+      setProjects(data.categories);
+    },
+  });
 
   useEffect(() => {
-    const { data, loading, error, refetch } = useQuery(GET_CATEGORIES);
-    if (loading) return "Loading";
-    if (error) return `Error! ${error.message}`;
-    setCategories(...data.categories);
-    return categories;
-  }, []);
+    refetch();
+  }, [projects]);
+
+  return { projects, setProjects };
 };
 
 export default useProjects;
--- apps/www/src/hooks/useTasks.js
+++ apps/www/src/hooks/useTasks.js
@@ -1,18 +1,31 @@
 import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
-import { GET_TASKS } from "@work-project/graphql";
+import { GET_CATEGORY_BY_NAME } from "@work-project/graphql";
+import { useSnackbar } from "notistack";
 
 const useTasks = selectedProject => {
+  const { enqueueSnackbar } = useSnackbar();
   const [tasks, setTasks] = useState([]);
   // const [archivedTasks, setArchivedTasks] = useState([]);
 
-  useEffect(() => {
-    const { data, loading, error, refetch } = useQuery(GET_TASKS);
-    if (loading) return "Loading";
-    if (error) return `Error! ${error.message}`;
-    setTasks(...data.tasks);
-    return tasks;
-  }, []);
+  let variables = {
+    withTasks: true,
+    name: selectedProject,
+  };
+
+  if (selectedProject === "INBOX") {
+    variables = {
+      withTasks: true,
+    };
+  }
+
+  const { data, error } = useQuery(GET_CATEGORY_BY_NAME, {
+    variables,
+    onError: error => enqueueSnackbar(error.message, { variant: "error" }),
+    onCompleted: () => setTasks(data.categoryByName?.tasks),
+  });
+
+  return { tasks, setTasks };
 };
 
 export default useTasks;
